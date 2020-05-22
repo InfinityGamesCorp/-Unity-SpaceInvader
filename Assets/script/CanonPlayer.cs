bbf22faeb02033b7111d8e83c9be13d737a7651f
Alex Robert <contact.alexrobert04@gmail.com>
2020-05-22 20:01:19 +0200

    Tir Player
     - Ajout system de tir du player
     - Projectile supprimer si sort de l'écran

--- Assets/script/CanonPlayer.cs
+++ Assets/script/CanonPlayer.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
 public class CanonPlayer : MonoBehaviour
 {
-
+    //Player
     GameObject canon;
     float speed = 0.08f;
+
+    //Shooter
+    public GameObject gm;
 
     void Start()
     {
@@ -22,5 +25,25 @@ public class CanonPlayer : MonoBehaviour
             canon.transform.position += new Vector3(speed, 0, 0);
         if (Input.GetKey(KeyCode.LeftArrow))
             canon.transform.position += new Vector3(-speed, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ProjectileShoot(canon.transform.position + new Vector3(0,0.4f,0));
+        }
+    }
+
+    public void ProjectileShoot(Vector3 position)
+    {
+        Instantiate(gm, position, Quaternion.identity);
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Mur_Droit")
+            canon.transform.position = new Vector3(8.3f, -4, 0);
     }
 }
---
+++ Assets/script/ShootPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootPlayer : MonoBehaviour
+{
+
+    void Start()
+    {
+    
+    }
+
+
+    void Update()
+    {
+        transform.position += new Vector3(0f, 0.05f, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
