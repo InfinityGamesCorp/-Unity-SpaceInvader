ae1b33322d5dc25ef4e680446b8105d55eecb2d9
Alex Robert <contact.alexrobert04@gmail.com>
2020-05-22 16:28:27 +0200

    Player
     - Ajout des déplacements du joueur (Gauche/Droite avec les flèche directionnels)

---
+++ Assets/script/CanonPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class CanonPlayer : MonoBehaviour
+{
+
+    GameObject canon;
+    float speed = 0.08f;
+
+    void Start()
+    {
+        canon = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            canon.transform.position += new Vector3(speed, 0, 0);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            canon.transform.position += new Vector3(-speed, 0, 0);
+    }
+}
